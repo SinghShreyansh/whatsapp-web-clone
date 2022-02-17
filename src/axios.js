6488ed87572bb8bbc786f786a0e0fef725b5659f
Shreyansh Singh <91277635+SinghShreyansh@users.noreply.github.com>
2022-02-17 18:09:44 +0530

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL:'http://localhost:9000'
+    baseURL:'https://whatsapp-web-server.herokuapp.com'
 });
 // http://localhost:9000
 // https://whatsapp-clone-mern-k70i8oihf-singhshreyansh.vercel.app
