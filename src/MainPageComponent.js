86dbbedcbca011940d0c1ee8e30ebc625340a4a1
Shreyansh Singh <91277635+SinghShreyansh@users.noreply.github.com>
2022-02-18 14:20:00 +0530

    Update MainPageComponent.js

--- src/MainPageComponent.js
+++ src/MainPageComponent.js
@@ -7,7 +7,8 @@ const MainPageComponent = ({LastMsg,RoomName}) => {
         e.preventDefault()
 
         document.getElementById("roomName").innerHTML= RoomName; 
-        document.getElementById("sideRoom").style.backgroundColor = "#EDEDED";
+//         document.getElementById("sideRoom").style.backgroundColor = "#EDEDED";
+        e.target.style.backgroundColor = "#ededed";
         
      }
     
